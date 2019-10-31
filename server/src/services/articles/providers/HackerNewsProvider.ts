302e1ae6d038623c6ae2c95a754a7fc8773ea92f
Nicolás Quezada <nexfor5@gmail.com>
2019-10-31 18:14:47 -0300

    Just stories

--- server/src/services/articles/providers/HackerNewsProvider.ts
+++ server/src/services/articles/providers/HackerNewsProvider.ts
@@ -2,7 +2,7 @@ import request from "request-promise";
 import {Article} from "../../../models/article";
 
 export async function getArticlesService(): Promise<Article[]> {
-    const url = `https://hn.algolia.com/api/v1/search_by_date?query=nodejs`;
+    const url = `https://hn.algolia.com/api/v1/search_by_date?query=nodejs&tags=story`;
     const response = JSON.parse(await request(url));
     return response.hits.map((article: any) => new Article(article));
 }
